a7d7d6fbb372df3f6d33ce9bdec830ebb0f4bec6
brandononeal <baoneal95@gmail.com>
2020-09-03 16:03:02 -0700

    mvp in progress

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,8 +1,19 @@
+import axios from 'axios'
+
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+
+axios.get('https://api.github.com/users/brandononeal')
+  .then(data => {
+    debugger
+    return data
+  })
+  .catch(err => {
+    debugger
+  })
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
@@ -16,6 +27,8 @@
   STEP 4: Pass the data received from Github into your function,
     and append the returned markup to the DOM as a child of .cards
 */
+
+
 
 /*
   STEP 5: Now that you have your own card getting added to the DOM, either
@@ -49,6 +62,50 @@ const followersArray = [];
       </div>
     </div>
 */
+
+function cardMaker(cardData){
+  const card = document.createElement('div')
+  const cardImg = document.createElement('img')
+  const cardInfo = document.createElement('div')
+  const cardName = document.createElement('h3')
+  const cardUserName = document.createElement('p')
+  const cardLocation = document.createElement('p')
+  const cardProfile = document.createElement('p')
+  const cardProfileLink = document.createElement('a')
+  const cardFollowers = document.createElement('p')
+  const cardFollowing = document.createElement('p')
+  const cardBio = document.createElement('p')
+
+  card.appendChild(cardImg)
+  card.appendChild(cardInfo)
+  cardInfo.appendChild(cardName)
+  cardInfo.appendChild(cardUserName)
+  cardInfo.appendChild(cardLocation)
+  cardInfo.appendChild(cardProfile)
+  cardProfile.appendChild(cardProfileLink)
+  cardInfo.appendChild(cardFollowers)
+  cardInfo.appendChild(cardFollowing)
+  cardInfo.appendChild(cardBio)
+
+  card.classList.add('card')
+  cardInfo.classList.add('card-info')
+  cardName.classList.add('name')
+  cardUserName.classList.add('username')
+
+  cardImg.src = cardData.avatar_url
+  cardName.textContent = cardData.name
+  cardUserName.textContent = cardData.login
+  cardLocation.textContent = cardData.location
+  cardProfileLink.href = cardData.html_url
+  cardProfileLink.textContent = cardData.html_url
+  cardFollowers.textContent = cardData.followers
+  cardFollowing.textContent = cardData.following
+  cardBio.textContent = cardData.bio
+  
+  return card
+}
+
+cardMaker()
 
 /*
   List of LS Instructors Github username's:
